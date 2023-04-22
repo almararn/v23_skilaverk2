b8cc740ee1f768602a8b15dc45c51d6b4b48801b
almararn <almararn@gmail.com>
2023-04-22 16:46:25 +0000

    1/3 finished

--- arrays.js
+++ arrays.js
@@ -8,8 +8,6 @@ console.log(getNthCharacterInArray([1, 2, 3, 4, 5], 3))
 console.log(getNthCharacterInArray([10, 9, 8, 7, 6], 5))
 console.log(getNthCharacterInArray([7, 2, 1, 6, 3], 1))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([1,2,3,4,5],3) Expected 3
@@ -17,8 +15,8 @@ myFunction([10,9,8,7,6],5) Expected 6
 myFunction([7,2,1,6,3],1) Expected 7
 */
 
+console.log("==============================")
 // ========================================
-
 // Write a function that takes an array (a) as argument
 // Remove the first 3 elements of 'a'
 // Return the result
@@ -31,8 +29,6 @@ console.log(removeFirstThreeElements([1, 2, 3, 4]))
 console.log(removeFirstThreeElements([5, 4, 3, 2, 1, 0]))
 console.log(removeFirstThreeElements([99, 1, 1]))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([1,2,3,4]) Expected [4]
@@ -40,8 +36,8 @@ myFunction([5,4,3,2,1,0]) Expected [2,1,0]
 myFunction([99,1,1]) Expected []
 */
 
+console.log("==============================")
 // ========================================
-
 // Write a function that takes an array (a) and a number (n) as arguments
 // It should return the last n elements of a
 // Tip: use the array prototype function slice()
@@ -53,8 +49,6 @@ console.log(removeLastNElements([1, 2, 3, 4, 5], 2))
 console.log(removeLastNElements([1, 2, 3], 6))
 console.log(removeLastNElements([1, 2, 3, 4, 5, 6, 7, 8], 3))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([1, 2, 3, 4, 5], 2) Expected [ 4, 5 ]
@@ -62,6 +56,7 @@ myFunction([1, 2, 3], 6) Expected [ 1, 2, 3 ]
 myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3) Expected [ 6, 7, 8 ]
 */
 
+console.log("==============================")
 // ========================================
 // Write a function that takes an array (a) as argument
 // Return the number of elements in a
@@ -74,8 +69,6 @@ console.log(countNumberOfElements([1, 2, 2, 4]))
 console.log(countNumberOfElements([9, 9, 9]))
 console.log(countNumberOfElements([4, 3, 2, 1, 0]))
 
-console.log("========================================")
-
 /*
 Test cases: 
 myFunction([1,2,2,4]) Expected 4
@@ -83,8 +76,8 @@ myFunction([9,9,9]) Expected 3
 myFunction([4,3,2,1,0]) Expected 5
 */
 
+console.log("==============================")
 // ========================================
-
 // Write a function that takes an array of numbers as argument
 // Return the number of negative values in the array
 // Tip: There are multiple ways to solve this e.g.
@@ -98,8 +91,6 @@ console.log(countNumberOfNegativeValues([1, -2, 2, -4]))
 console.log(countNumberOfNegativeValues([0, 9, 1]))
 console.log(countNumberOfNegativeValues([4, -3, 2, 1, 0]))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([1,-2,2,-4]) Expected 2
@@ -107,8 +98,8 @@ myFunction([0,9,1]) Expected 0
 myFunction([4,-3,2,1,0]) Expected 1
 */
 
+console.log("==============================")
 // ========================================
-
 // Write a function that takes an array of numbers as argument
 // It should return the sum of the numbers
 // Tip: forEach, c-style loop (or even .reduce() for the brave)
@@ -124,8 +115,6 @@ console.log(calcSumOfArrayOfNumbers([10, 100, 40]))
 console.log(calcSumOfArrayOfNumbers([10, 100, 1000, 1]))
 console.log(calcSumOfArrayOfNumbers([-50, 0, 50, 200]))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([10,100,40]) Expected 150
@@ -133,6 +122,7 @@ myFunction([10,100,1000,1]) Expected 1111
 myFunction([-50,0,50,200]) Expected 200
 */
 
+console.log("==============================")
 // ========================================
 // Write a function that takes an array of numbers as argument
 // It should return the average of the numbers
@@ -150,8 +140,6 @@ console.log(calcAvgOfArrayOfNumbers([10, 100, 40]))
 console.log(calcAvgOfArrayOfNumbers([10, 100, 1000]))
 console.log(calcAvgOfArrayOfNumbers([-50, 0, 50, 200]))
 
-console.log("========================================")
-
 /*
 Test cases:
 myFunction([10,100,40]) Expected 50
@@ -159,6 +147,7 @@ myFunction([10,100,1000]) Expected 370
 myFunction([-50,0,50,200]) Expected 50
 */
 
+console.log("==============================")
 // ========================================
 // Write a function that takes an array of strings as argument
 // Return the longest string
@@ -171,10 +160,8 @@ const getLongestStringFromArray = (arr) => {
   return longest
 }
 
-console.log(getLongestStringFromArray(['help', 'me']))
-console.log(getLongestStringFromArray(['I', 'need', 'candy']))
-
-console.log("========================================")
+console.log(getLongestStringFromArray(["help", "me"]))
+console.log(getLongestStringFromArray(["I", "need", "candy"]))
 
 /*
 Test cases:
@@ -182,6 +169,7 @@ myFunction(['help', 'me']) Expected 'help'
 myFunction(['I', 'need', 'candy']) Expected 'candy'
 */
 
+console.log("==============================")
 // ========================================
 // Write a function that takes an array as argument
 // It should return true if all elements in the array are equal
@@ -190,7 +178,7 @@ myFunction(['I', 'need', 'candy']) Expected 'candy'
 const areAllEqual = (arr) => {
   let isEqual = true
   arr.forEach((element) => {
-    if(arr[0] !== element){
+    if (arr[0] !== element) {
       isEqual = false
     }
   })
@@ -198,11 +186,9 @@ const areAllEqual = (arr) => {
 }
 
 console.log(areAllEqual([true, true, true, true]))
-console.log(areAllEqual(['test', 'test', 'test']))
-console.log(areAllEqual([1,1,1,2]))
-console.log(areAllEqual(['10',10,10,10]))
-
-console.log("========================================")
+console.log(areAllEqual(["test", "test", "test"]))
+console.log(areAllEqual([1, 1, 1, 2]))
+console.log(areAllEqual(["10", 10, 10, 10]))
 
 /*
 Test cases:
@@ -211,21 +197,23 @@ myFunction(['test', 'test', 'test']) Expected true
 myFunction([1,1,1,2]) Expected false 
 myFunction(['10',10,10,10]) Expected false 
 */
+
+console.log("==============================")
 // ========================================
 // Write a function that takes arguments an arbitrary number of arrays
 // It should return an array containing the values of all arrays
 // Tip: Make use of the spread syntax (...), as the parameters suggest
 const mergeAllArrays = (...arrays) => {
   let mergedArray = []
-  arrays.forEach(element => {
+  arrays.forEach((element) => {
     mergedArray.push(...element)
-  });
+  })
   return mergedArray
 }
 
 console.log(mergeAllArrays([1, 2, 3], [4, 5, 6]))
-console.log(mergeAllArrays(['a', 'b', 'c'], [4, 5, 6]))
-console.log(mergeAllArrays([true, true], [1, 2], ['a', 'b']))
+console.log(mergeAllArrays(["a", "b", "c"], [4, 5, 6]))
+console.log(mergeAllArrays([true, true], [1, 2], ["a", "b"]))
 
 /*
 Test cases:
