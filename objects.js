3825671ba72e824f9641b457b1a2702f42d7e1cb
almararn <almararn@gmail.com>
2023-04-25 18:44:27 +0000

    obj @ 50%

--- objects.js
+++ objects.js
@@ -3,8 +3,13 @@
 // In other words, it should not be null or undefined or false
 // Return false otherwise
 const checkIfPropertyExistsAndTruthy = (a, b) => {
-  return;
+  return a.hasOwnProperty(b) && Boolean(a[b]);
 };
+
+console.log(checkIfPropertyExistsAndTruthy({a:1,b:2,c:3},'b'))
+console.log(checkIfPropertyExistsAndTruthy({x:'a',y:null,z:'c'},'y'))
+console.log(checkIfPropertyExistsAndTruthy({x:'a',b:'b',z:undefined},'z'))
+
 /*
 Test cases:
 myFunction({a:1,b:2,c:3},'b') Expected true
@@ -12,13 +17,17 @@ myFunction({x:'a',y:null,z:'c'},'y') Expected false
 myFunction({x:'a',b:'b',z:undefined},'z') Expected false
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes an object with two properties as argument
 // It should return the value of the property with key country
 const getCountry = (obj) => {
-  return;
+  return obj.country;
 };
+
+console.log(getCountry({ continent: 'Asia', country: 'Japan' }))
+console.log(getCountry({ country: 'Sweden', continent: 'Europe' }))
 
 /*
 Test cases:
@@ -26,14 +35,18 @@ myFunction({ continent: 'Asia', country: 'Japan' }) Expected 'Japan'
 myFunction({ country: 'Sweden', continent: 'Europe' }) Expected 'Sweden'
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes an object with two properties as argument
 // It should return the value of the property with key 'prop-2'
 // Tip: you might want to use the square brackets to access the property
 const getWeirdKeyValue = (obj) => {
-  return;
+  return obj['prop-2'];
 };
+
+console.log(getWeirdKeyValue({  one: 1,  'prop-2': 2}))
+console.log(getWeirdKeyValue({  'prop-2': 'two',  prop: 'test'}))
 
 /*
 Test cases:
@@ -41,13 +54,17 @@ myFunction({  one: 1,  'prop-2': 2}) Expected 2
 myFunction({  'prop-2': 'two',  prop: 'test'}) Expected 'two'
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes an object with two properties and a string as arguments
 // It should return the value of the property with key equal to the value of the string
 const getPropertyByString = (obj, key) => {
-  return;
+  return obj[key];
 };
+
+console.log(getPropertyByString({  continent: 'Asia',  country: 'Japan'}, 'continent'))
+console.log(getPropertyByString({  country: 'Sweden',  continent: 'Europe'}, 'country'))
 
 /*
 Test cases:
@@ -55,6 +72,7 @@ myFunction({  continent: 'Asia',  country: 'Japan'}, 'continent') Expected 'Asia
 myFunction({  country: 'Sweden',  continent: 'Europe'}, 'country') Expected 'Sweden'
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes an object (a) and a string (b) as argument
@@ -62,8 +80,12 @@ myFunction({  country: 'Sweden',  continent: 'Europe'}, 'country') Expected 'Swe
 // Return false otherwise
 // NOTE: Test case 3 is a bit tricky because the value of property 'z' is undefined, but the property itself exists
 const checkIfPropertyExists = (a, b) => {
-  return;
+  return a.hasOwnProperty(b);
 };
+
+console.log(checkIfPropertyExists({a:1,b:2,c:3},'b'))
+console.log(checkIfPropertyExists({x:'a',y:'b',z:'c'},'a'))
+console.log(checkIfPropertyExists({x:'a',y:'b',z:undefined},'z'))
 
 /*
 Test cases:
@@ -72,14 +94,19 @@ myFunction({x:'a',y:'b',z:'c'},'a') Expected false
 myFunction({x:'a',y:'b',z:undefined},'z') Expected true
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes a string as argument
 // Create an object that has a property with key 'key' and a value equal to the string
 // Return the object
 const createObjectWithKeyValue = (a) => {
-  return;
+  return { key: a};
 };
+
+console.log(createObjectWithKeyValue('a'))
+console.log(createObjectWithKeyValue('z'))
+console.log(createObjectWithKeyValue('b'))
 
 /*
 Test cases:
@@ -88,14 +115,19 @@ myFunction('z') Expected {key:'z'}
 myFunction('b') Expected {key:'b'}
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes two strings (a and b) as arguments
 // Create an object that has a property with key 'a' and a value of 'b'
 // Return the object
 const createObjectWithKeyAndValue = (a, b) => {
-  return;
+  return {a: b};
 };
+
+console.log(createObjectWithKeyAndValue('a','b'))
+console.log(createObjectWithKeyAndValue('z','x'))
+console.log(createObjectWithKeyAndValue('b','w'))
 
 /*
 Test cases:
@@ -104,14 +136,23 @@ myFunction('z','x') Expected {z:'x'}
 myFunction('b','w') Expected {b:'w'}
 */
 
+console.log("==============================")
 // ========================================
 
 // Write a function that takes two arrays (a and b) as arguments
 // Create an object that has properties with keys 'a' and corresponding values 'b'
 // Return the object
 const createObjectFromArrays = (a, b) => {
-  return;
+  let object = {}
+  a.forEach((value,i) => {
+    object[value] = b[i]
+  });
+  return object;
 };
+
+console.log(createObjectFromArrays(['a','b','c'],[1,2,3]))
+console.log(createObjectFromArrays(['w','x','y','z'],[10,9,5,2]))
+console.log(createObjectFromArrays([1,'b'],['a',2]))
 
 /*
 Test cases:
@@ -120,6 +161,7 @@ myFunction(['w','x','y','z'],[10,9,5,2]) Expected {w:10,x:9,y:5,z:2}
 myFunction([1,'b'],['a',2]) Expected {1:'a',b:2}
 */
 
+console.log("==============================")
 // ========================================
 // Write a function that takes an object (a) as argument
 // Return an array with all object keys
